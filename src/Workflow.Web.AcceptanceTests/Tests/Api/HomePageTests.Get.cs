2d707e6751d52e4bb27b859306154a61c08d124d
TehWardy <paul.ward@ccoder.co.uk>
2026-07-01 20:38:41 +0100

    Align Workflow tester UI

--- src/Workflow.Web.AcceptanceTests/Tests/Api/HomePageTests.Get.cs
+++ src/Workflow.Web.AcceptanceTests/Tests/Api/HomePageTests.Get.cs
@@ -1,4 +1,5 @@
 using FluentAssertions;
+using System.Net;
 using Xunit;
 
 namespace Web.AcceptanceTests.Tests.Api;
@@ -6,15 +7,30 @@ namespace Web.AcceptanceTests.Tests.Api;
 public sealed partial class HomePageTests
 {
     [Fact]
-    public async Task Get_ReturnsWorkflowTester()
+    public async Task Get_RedirectsToWorkflowTester()
     {
         // Given
 
         // When
-        string actualHome = await GetHomeAsync();
+        using HttpResponseMessage response = await GetHomeAsync();
+
+        // Then
+        response.StatusCode.Should().Be(HttpStatusCode.Redirect);
+        response.Headers.Location?.OriginalString.Should().Be("/tools/index.html");
+    }
+
+    [Fact]
+    public async Task GetTools_ReturnsWorkflowTester()
+    {
+        // Given
+
+        // When
+        string actualHome = await GetToolsAsync();
 
         // Then
         actualHome.Should().Contain("<title>Workflow</title>");
-        actualHome.Should().Contain("flowManagement.js");
+        actualHome.Should().Contain("/tools/api.js");
+        actualHome.Should().Contain("/tools/grids.js");
+        actualHome.Should().Contain("Flow definition and execution management");
     }
 }
