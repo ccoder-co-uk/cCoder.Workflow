11b6974bcb94a12fef632078152b22fc3bab86f3
TehWardy <paul.ward@ccoder.co.uk>
2026-07-07 13:04:08 +0100

    Add Workflow baseline endpoint

---
+++ src/Workflow.Web.AcceptanceTests/Tests/Api/BaselineTests.Get.cs
@@ -0,0 +1,16 @@
+using System.Text.Json;
+using FluentAssertions;
+using Xunit;
+
+namespace Web.AcceptanceTests.Tests.Api;
+
+public sealed partial class BaselineTests
+{
+    [Fact]
+    public async Task Get_GivenBaselineEndpoint_ShouldReturnPackagesArray()
+    {
+        JsonElement baseline = await GetBaselineAsync();
+
+        baseline.ValueKind.Should().Be(JsonValueKind.Array);
+    }
+}
---
+++ src/cCoder.Workflow/Exposures/Setup/WorkflowBaselinePackages.cs
@@ -0,0 +1,8 @@
+using cCoder.Data.Models.Packaging;
+
+namespace cCoder.Workflow.Exposures.Setup;
+
+public static class WorkflowBaselinePackages
+{
+    public static Package[] All => [];
+}
