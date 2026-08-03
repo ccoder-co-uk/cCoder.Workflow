de765311bb35ebfc2c67f30549273d9a15fb71a8
TehWardy <paul.ward@ccoder.co.uk>
2026-08-04 00:23:49 +0100

    Expose workflow instances through manager

--- src/cCoder.Workflow/Exposures/IWorkflowInstanceManager.cs
+++ src/cCoder.Workflow/Exposures/IWorkflowInstanceManager.cs
@@ -2,6 +2,8 @@
 // Copyright (c) Paul.Ward@ccoder.co.uk
 // ---------------------------------------------------------------
 
+using cCoder.Data.Models.Workflow;
+
 namespace cCoder.Workflow.Exposures;
 
 public interface IWorkflowInstanceManager
@@ -11,6 +13,7 @@ public interface IWorkflowInstanceManager
     Task RunInstanceMaintenanceAsync(CancellationToken cancellationToken = default);
     Task RunQueueInstanceBackgroundServiceDependencyContinuouslyAsync(CancellationToken cancellationToken = default);
     Task RunQueueInstanceBackgroundServiceDependencyAsync(CancellationToken cancellationToken = default);
+    IQueryable<FlowInstanceData> GetAll(bool ignoreFilters = false);
     object[] GetStats();
     ValueTask ExecuteWaitingQueuedInstanceByIdAsync(Guid flowInstanceDataId);
 }
